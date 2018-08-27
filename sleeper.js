e02ca7e003459f4de55c100c79c5d90d6fce26ba
David McQueen <david@dmcqueen.co.uk>
2018-08-27 20:13:07 +0100

    Add in basic styling & timer input

--- sleeper.js
+++ sleeper.js
@@ -6,15 +6,53 @@ window.netflixSleeper.extensionControl = (function() {
         currentTab: undefined
     };
 
+    var timerControls = {
+        incrementTime: function() {
+            var currentTime = timerControls.getSleepTimeMS();
+            var newTime = currentTime + 1;
+
+            if(newTime > 18000) // Limited to 5 hours
+                newTime = 18000;
+
+            timerControls.setSleepTimeMS(newTime);
+        },
+        decrementTime: function() {
+            var currentTime = timerControls.getSleepTimeMS();
+            var newTime = currentTime - 1;
+
+            if(newTime < 0) // Can't have negative sleep time
+                newTime = 0;
+
+            timerControls.setSleepTimeMS(newTime);
+        },
+        getSleepTimeMS: function() {
+            var timerInput = document.getElementById("sleepTime");
+
+            return parseInt(timerInput.value) || 0;
+        },
+        setSleepTimeMS: function(time){
+            var timerInput = document.getElementById("sleepTime");
+            timerInput.value = time || 0;
+        }
+    }
+
     var controls = {
         setup: function(info) {
             vars.currentTab = info;
     
-            document.getElementById("playButton").addEventListener("click", controls.setupPlayDelay);
+            controls.setupTimerButtons();
+            controls.setupTimerControlButtons();
+        },
+        setupTimerControlButtons: function() {
+            document.getElementById("decrementTime").addEventListener("click", timerControls.decrementTime);
+            document.getElementById("incrementTime").addEventListener("click", timerControls.incrementTime);
+        },
+        setupTimerButtons: function() {
+            // document.getElementById("playButton").addEventListener("click", controls.setupPlayDelay);
             document.getElementById("pauseButton").addEventListener("click", controls.setupPauseDelay);
         },
         setupPlayDelay: function() {
-            var sleepTime = controls.getSleepTimeMS();
+            var sleepTime = timerControls.getSleepTimeMS();
 
             chrome.tabs.executeScript(vars.currentTab.id, {
                 code: 'var sleepTime = ' + sleepTime + ';'
@@ -23,7 +61,7 @@ window.netflixSleeper.extensionControl = (function() {
             });
         },
         setupPauseDelay: function() {
-            var sleepTime = controls.getSleepTimeMS();
+            var sleepTime = timerControls.getSleepTimeMS();
 
             chrome.tabs.executeScript(vars.currentTab.id, {
                 code: 'var sleepTime = ' + sleepTime + ';'
@@ -31,25 +69,27 @@ window.netflixSleeper.extensionControl = (function() {
                 chrome.tabs.executeScript(vars.currentTab.id, {file: "pause_content_script.js"});
             });
         },
-        getSleepTimeMS: function() {
-            debugger;
-            return 5000;
-        },
         setupEventListener: function() {
             window.addEventListener('DOMContentLoaded', function () {
 
-                chrome.tabs.query({
-                    active: true,
-                    currentWindow: true
-                  }, function (tabs) {
-                      debugger;
-                      controls.setup(tabs[0]);
-                  });
+                if(chrome.tabs){
+                    chrome.tabs.query({
+                        active: true,
+                        currentWindow: true
+                      }, function (tabs) {
+                          debugger;
+                          controls.setup(tabs[0]);
+                      });
+                }
+                else{
+                    controls.setup();
+                    return;
+                }
+
             
             });
         }
     };
-    debugger;
 
     controls.setupEventListener();
 
